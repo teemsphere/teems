037af068bd97e862efaaba01cb7b389b7989fd4a
Matthew Cantele <m.cantele@unimelb.edu.au>
2025-11-11 11:51:43 +1300

    chk statement logic improved

--- R/chk_statements.R
+++ R/chk_statements.R
@@ -1,47 +1,45 @@
+#' @importFrom purrr map_chr
+#' 
 #' @keywords internal
+#' @note Comprehensive statement-specific checks to be implemented here
 #' @noRd
 .check_statements <- function(tab,
                               ok_state,
                               call) {
+  n_comments <- paste(unlist(strsplit(tab, "![^!]*!", perl = TRUE)), collapse = "")
+  statements <- unlist(strsplit(n_comments, ";", perl = TRUE))
 
-  # remove comments
-  n_comments <- paste(unlist(x = strsplit(x = tab, "![^!]*!", perl = TRUE)), collapse = "")
-  statements <- unlist(x = strsplit(x = n_comments, split = ";", perl = TRUE))
-  
-  # remove new lines and carriage returns
-  statements <- gsub(pattern = "\r|\n",
-                     replacement = " ",
-                     x = statements,
-                     perl = TRUE)
-  statements <- gsub(pattern = "\\s{2,}",
-                     replacement = " ",
-                     x = statements)
-  statements <- trimws(x = statements[statements != " "])
-  
-  # check whether statements implicit
-  state_decl <- unique(x = sapply(X = strsplit(x = statements, split = " ", perl = TRUE), "[[", 1))
-  
-  if (any(!is.element(el = state_decl, set = ok_state))) {
+  statements <- gsub("\r|\n", " ", statements, perl = TRUE)
+  statements <- gsub("\\s{2,}", " ", statements)
+  statements <- trimws(statements[statements != " "])
+
+  state_decl <- tolower(unique(purrr::map_chr(strsplit(statements, " ", perl = TRUE), 1)))
+
+  if (any(!state_decl %in% tolower(ok_state))) {
     # make implicit decl explicit
-    for (s in seq_len(length(x = statements))) {
+    for (s in seq_len(length(statements))) {
       statement <- strsplit(statements[s], split = " ")[[1]][1]
-      if (!grepl(pattern = paste(ok_state, collapse = "|"),
-                 x = statement,
-                 ignore.case = TRUE)) {
-        implicit_stat <- strsplit(statements[s-1], split = " ")[[1]][1]
+      state_check <- paste0("\\b", ok_state, "\\b")
+      if (!grepl(paste(state_check, collapse = "|"),
+        statement,
+        ignore.case = TRUE
+      )) {
+        implicit_stat <- strsplit(statements[s - 1], split = " ")[[1]][1]
         statements[s] <- paste(implicit_stat, statements[s])
       }
     }
   }
-  
+
   # second check
-  state_decl <- unique(x = sapply(X = strsplit(x = statements, split = " ", perl = TRUE), "[[", 1))
-  if (any(!is.element(el = tolower(x = state_decl), set = tolower(x = ok_state)))) {
-    unsupported <- state_decl[is.element(el = tolower(x = state_decl), set = tolower(x = ok_state))]
+  state_decl <- tolower(unique(purrr::map_chr(strsplit(statements, " ", perl = TRUE), 1)))
+  if (any(!state_decl %in% tolower(ok_state))) {
+    state_decl <- unique(purrr::map_chr(strsplit(statements, " ", perl = TRUE), 1))
+    unsupported <- state_decl[!tolower(state_decl) %in% tolower(ok_state)]
     .cli_action(model_err$unsupported_tab,
-                action = "abort",
-                call = call)
+      action = "abort",
+      call = call
+    )
   }
-  
+
   return(statements)
 }

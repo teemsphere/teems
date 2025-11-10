cce3f25a8b7b2f9a48ffc73e7015850d0c898586
Matthew Cantele <m.cantele@unimelb.edu.au>
2025-11-11 12:17:01 +1300

    proper space parsing

--- R/tab_maths_parse.R
+++ R/tab_maths_parse.R
@@ -1,70 +1,61 @@
-#' @importFrom purrr map2 pmap
+#' @importFrom purrr map2 pmap map map_chr pluck
 #' 
 #' @keywords internal
+#' @note Replace all sapply with purrr::map
 #' @noRd
 .parse_tab_maths <- function(tab_extract,
                              call) {
-
   maths <- subset(tab_extract, tolower(type) %in% c("equation", "formula"))
 
-  maths[["name"]] <- unlist(x = purrr::map2(
-    .x = maths[["type"]],
-    .y = maths[["remainder"]],
-    .f = function(t, r) {
-      if (identical(x = t, y = "Equation")) {
-        lapply(X = strsplit(x = r, split = " "), "[[", 1)
+  maths$name <- unlist(purrr::map2(
+    maths$type,
+    maths$remainder,
+    function(t, r) {
+      if (t %=% "Equation") {
+        lapply(strsplit(r, " "), "[[", 1)
       } else {
         NA
       }
     }
   ))
-  # info check
-  # equations <- maths$remainder[maths$type == "Equation"]
-  # if (!all(lengths(gregexpr("#", equations, fixed = TRUE)) == 2)) {
-  #   # missing label
-  #   m_eq <- equations[lengths(x = gregexpr("#", equations, fixed = TRUE)) != 2]
-  #   .cli_action(msg = "The following equations are missing a label: 
-  #               {.val {m_eq}}. For no label use e.g., # NA #.",
-  #               action = "inform")
-  # }
-  
-  maths[["remainder"]] <- unlist(x = purrr::map2(
-    .x = maths[["remainder"]],
-    .y = maths[["name"]],
-    .f = function(r, n) {
-      if (!is.na(x = n)) {
-        return(trimws(sub(pattern = n, "", r, fixed = TRUE)))
+
+  maths$remainder <- unlist(purrr::map2(
+    maths$remainder,
+    maths$name,
+    function(r, n) {
+      if (!is.na(n)) {
+        return(trimws(sub(n, "", r, fixed = TRUE)))
       } else {
         return(r)
       }
     }
   ))
 
-  maths[["label"]] <- sapply(
-    X = strsplit(
-      x = maths[["remainder"]],
+  maths$label <- sapply(
+    strsplit(
+      maths$remainder,
       split = "#"
     ),
     FUN = function(spl) {
-      if (length(x = spl) > 1) {
-        trimws(x = spl[2])
+      if (length(spl) > 1) {
+        trimws(spl[2])
       } else {
         NA
       }
     }
   )
 
-  maths[["remainder"]] <- unlist(x = pmap(
-    .l = list(
-      maths[["remainder"]],
-      maths[["label"]],
-      maths[["type"]]
+  maths$remainder <- unlist(purrr::pmap(
+    list(
+      maths$remainder,
+      maths$label,
+      maths$type
     ),
-    .f = function(rem, info, t) {
-      if (identical(x = t, y = "Equation")) {
-        if (grepl(pattern = "#", x = rem)) {
-        rem <- strsplit(x = rem, split = "#")[[1]][3]
-        trimws(x = rem)
+    function(rem, info, t) {
+      if (t %=% "Equation") {
+        if (grepl(pattern = "#", rem)) {
+          rem <- strsplit(rem, "#")[[1]][3]
+          trimws(rem)
         } else {
           return(rem)
         }
@@ -73,8 +64,7 @@
       }
     }
   ))
-  
-  # from util_tablo_var (make function)
+
   qual <- c(
     "nonzero_by_zero",
     "zero_by_zero",
@@ -89,50 +79,57 @@
     "\\breal\\b",
     "levels"
   )
-  
-  # first parenthetic enclosure which may include a qualifier
-  first_enclosure <- lapply(X = strsplit(x = maths[["remainder"]], ")"), "[[", 1)
-  
-  maths[["qualifier_list"]] <- ifelse(test = grepl(
-    pattern = paste(qual, collapse = "|"),
-    x = first_enclosure,
-    ignore.case = TRUE
-  ),
-  yes = paste0(first_enclosure, ")"),
-  no = NA
-  )
-  
-  maths[["remainder"]] <- .advance_remainder(
-    remainder = maths[["remainder"]],
-    pattern = maths[["qualifier_list"]]
-  )
-  
-  maths[["full_set"]] <- sapply(X = maths[["remainder"]],
-                                FUN = .cleave_maths,
-                                pos = 2L,
-                                USE.NAMES = FALSE)
-  
-  maths[["math"]] <- sapply(X = maths[["remainder"]],
-                            FUN = .cleave_maths,
-                            pos = 3L,
-                            USE.NAMES = FALSE)
 
-  maths[["math"]] <- ifelse(test = is.na(x = maths[["full_set"]]),
-                            yes = maths[["remainder"]],
-                            no = maths[["math"]])
-  
-  maths[["LHS"]] <- trimws(x = sapply(X = strsplit(x = maths[["math"]],
-                                                   split = "="),
-                                      FUN = "[[", 1))
-  
-  maths[["RHS"]] <- trimws(x = sapply(X = strsplit(x = maths[["math"]],
-                                                   split = "="),
-                                      FUN = "[[", 2))
-  
+  first_enclosure <- lapply(strsplit(maths$remainder, ")"), "[[", 1)
 
-  maths[["full_set"]] <- ifelse(test = is.na(x = maths[["full_set"]]),
-                                yes = "null_set",
-                                no = maths[["full_set"]])
-  
+  maths$qualifier_list <- ifelse(grepl(paste(qual, collapse = "|"), first_enclosure, ignore.case = TRUE),
+    paste0(first_enclosure, ")"),
+    NA
+  )
+
+  maths$remainder <- .advance_remainder(
+    remainder = maths$remainder,
+    pattern = maths$qualifier_list
+  )
+
+  maths$full_set <- purrr::map_chr(maths$remainder, function(r) {
+    return_comp <- regmatches(
+      r,
+      regexec(
+        "^((?:\\([^()]*(?:\\([^()]*\\)[^()]*)*\\)\\s*)+)(.*)$",
+        r
+      )
+    )
+    return_comp <- purrr::pluck(return_comp, 1, 2)
+    if (is.null(return_comp)) {
+      return_comp <- NA
+    } else {
+      return_comp <- gsub("\\s", "", trimws(return_comp))
+    }
+    return(return_comp)
+  })
+
+  maths$math <- purrr::map_chr(maths$remainder, function(r) {
+    return_comp <- regmatches(
+      r,
+      regexec(
+        "^((?:\\([^()]*(?:\\([^()]*\\)[^()]*)*\\)\\s*)+)(.*)$",
+        r
+      )
+    )
+    return_comp <- purrr::pluck(return_comp, 1, 3)
+
+    if (is.null(return_comp)) {
+      return_comp <- r
+    } else {
+      return_comp <- trimws(return_comp)
+    }
+    return(return_comp)
+  })
+
+
+  maths$LHS <- trimws(purrr::map_chr(purrr::map(maths$math, strsplit, split = "="), purrr::pluck, 1, 1))
+  maths$RHS <- trimws(purrr::map_chr(purrr::map(maths$math, strsplit, split = "="), purrr::pluck, 1, 2))
+
   return(maths)
 }

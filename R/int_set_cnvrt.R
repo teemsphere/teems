251e4e27816f9759828341c3e266330a9eef86e5
Matthew Cantele <m.cantele@unimelb.edu.au>
2025-11-11 13:41:17 +1300

    proper coeff/header usage

--- R/int_set_cnvrt.R
+++ R/int_set_cnvrt.R
@@ -4,50 +4,53 @@
 #' @noRd
 .convert_int_sets <- function(expr,
                               n_timestep,
-                              n_timestep_header) {
+                              n_timestep_coeff) {
 
-  terms <- strsplit(x = expr, split = "-|\\s*-\\s*")[[1]]
-  if (grepl(
-    pattern = "P\\[.*\\]\\s*-\\s*P\\[",
-    x = expr
-  )) {
-    terms <- strsplit(
-      x = expr,
-      split = "\\s*-\\s*(?=P\\[)",
+  terms <- strsplit(expr, "-|\\s*-\\s*")[[1]]
+  if (grepl("P\\[.*\\]\\s*-\\s*P\\[", expr)) {
+    terms <- strsplit(expr, "\\s*-\\s*(?=P\\[)",
       perl = TRUE
     )[[1]]
-    start <- .convert_p_term(term = terms[1],
-                             n_timestep = n_timestep,
-                             n_timestep_header = n_timestep_header)
-    end <- .convert_p_term(term = terms[2],
-                           n_timestep = n_timestep,
-                           n_timestep_header = n_timestep_header)
+    start <- .convert_p_term(
+      term = terms[1],
+      n_timestep = n_timestep,
+      n_timestep_coeff = n_timestep_coeff
+    )
+    end <- .convert_p_term(
+      term = terms[2],
+      n_timestep = n_timestep,
+      n_timestep_coeff = n_timestep_coeff
+    )
     num_vec <- c(start:end)
   } else {
-    num_vec <- .convert_p_term(term = expr,
-                               n_timestep = n_timestep,
-                               n_timestep_header = n_timestep_header)
+    num_vec <- .convert_p_term(
+      term = expr,
+      n_timestep = n_timestep,
+      n_timestep_coeff = n_timestep_coeff
+    )
   }
-  
-  mapping <- data.table::data.table(origin = num_vec,
-                                    mapping = num_vec,
-                                    key = c("origin", "mapping"))
+
+  mapping <- data.table::data.table(
+    origin = num_vec,
+    mapping = num_vec,
+    key = c("origin", "mapping")
+  )
   return(mapping)
 }
 
 .convert_p_term <- function(term,
                             n_timestep,
-                            n_timestep_header) {
+                            n_timestep_coeff) {
 
   content <- gsub(
-    pattern = "P\\[|\\]",
-    replacement = "",
-    x = term
+    "P\\[|\\]",
+    "",
+    term
   )
   content <- gsub(
-    pattern = n_timestep_header,
-    replacement = as.character(x = n_timestep),
-    x = content
+    n_timestep_coeff,
+    as.character(n_timestep),
+    content
   )
-  eval(expr = parse(text = content))
+  eval(parse(text = content))
 }

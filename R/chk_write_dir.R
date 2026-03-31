85e3c0a67c1b1016b919e9479ae0b51abc368014
Matthew Cantele <m.cantele@unimelb.edu.au>
2026-04-01 11:41:22 +1300

    chk_write_dir: leaf-only create; normalizePath unconditional; writability check; inform on wipe; return sub_path

--- R/chk_write_dir.R
+++ R/chk_write_dir.R
@@ -1,29 +1,42 @@
-#' @importFrom tools R_user_dir
-#' 
 #' @keywords internal
 #' @noRd
 .check_write_dir <- function(write_dir,
                              call) {
-
   subdir <- .o_write_sub_dir()
-  if (write_dir %!=% tools::R_user_dir("teems", "cache")) {
-    write_dir <- normalizePath(write_dir)
-    if (!dir.exists(write_dir)) {
+
+  if (!dir.exists(write_dir)) {
+    created <- dir.create(write_dir, recursive = FALSE)
+    if (!created) {
       .cli_action(deploy_err$invalid_write_dir,
         action = "abort",
         call = call
       )
     }
-
-    write_dir <- file.path(write_dir, subdir)
   }
 
-  unlink(write_dir,
-    recursive = TRUE
-  )
+  write_dir <- normalizePath(write_dir)
 
-  dir.create(write_dir,
-    recursive = TRUE
-  )
-  return(write_dir)
+  if (file.access(write_dir, 2) != 0L) {
+    .cli_action(deploy_err$write_dir_not_writable,
+      action = "abort",
+      call = call
+    )
+  }
+
+  sub_path <- file.path(write_dir, subdir)
+
+  if (dir.exists(sub_path)) {
+    existing <- list.files(sub_path, all.files = TRUE, no.. = TRUE)
+    if (length(existing) > 0) {
+      .cli_action("Clearing {length(existing)} existing file{?s} from {.path {sub_path}}",
+        action = "inform",
+        call = call
+      )
+      unlink(file.path(sub_path, "*"), expand = TRUE)
+    }
+  } else {
+    dir.create(sub_path, recursive = FALSE)
+  }
+
+  return(sub_path)
 }

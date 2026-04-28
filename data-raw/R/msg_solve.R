498c62e5bb406125d11480961c1b7c8712bff329
Matthew Cantele <m.cantele@unimelb.edu.au>
2026-04-29 11:24:58 +1200

    shorten error message strings; add no_model_dir error; update terminal_run step to reference cli_verbatim output; fix grammar in accuracy messages

--- data-raw/R/msg_solve.R
+++ data-raw/R/msg_solve.R
@@ -1,32 +1,33 @@
 build_solve_err <- function() {
   list(
-    no_insitu_inputs = c("No input files have been loaded.",
-                              "All input files must be passed as named arguments using {.arg ...} under the {.emph in-situ solve} method."),
-    missing_insitu_inputs = "The Tablo file provided indicates that the following files are required: {.val {req_inputs}} however one or more appear to not have been provided: {.val {missing_files}}.",
-    insitu_no_file = "One or more input files provided does not exist: {.val {nonexist_files}}.",
+    no_insitu_inputs = "No input files loaded; all files must be passed as named arguments via {.arg ...}.",
+    missing_insitu_inputs = "Required files {.val {req_inputs}} not all provided; missing: {.val {missing_files}}.",
+    insitu_no_file = "Input file{?s} not found: {.val {nonexist_files}}.",
     # test-ems_solve.R: "ems_solve errors when n_tasks is not integerish"
     x_integerish = "{.arg {arg}} must be integer-like.",
     invalid_length = "{.arg {arg}} must be an integer-like numeric of length 1.",
     # test-ems_solve.R: "ems_solve errors when steps are mixed odd/even"
-    subint_form = "{.arg n_subintervals} must contain either all even numbers or all odd numbers.",
+    subint_form = "{.arg n_subintervals} must be all even or all odd.",
     # test-ems_solve.R: "ems_solve errors when steps is not length 3"
     step_length = "{.arg steps} must be a numeric vector of length 3.",
     # test-ems_solve.R: "ems_solve errors when SBBD used with static model"
     invalid_method = "{.arg matrix_method} {.val {matrix_method}} only applicable to intertemporal model runs.",
     solution_err = "Errors detected during solution. See {.path {paths$diag_out}}.",
     solution_sing = "Singularity detected during solution. See {.path {paths$diag_out}}.",
-    docker_installed = "Docker is required to call the solver and is not installed on this system.",
+    # not simulated
+    docker_installed = "Docker is required but not installed.",
     docker_sudo = "Docker is installed but cannot be called without sudo.",
     docker_not_running = "Docker is installed but the daemon is not running. Start Docker Desktop and try again.",
-    docker_x_image = "The {.val {image_name}} Docker image is not present."
+    docker_x_image = "The {.val {image_name}} Docker image is not present.",
+    no_model_dir = "The {.arg model_dir} provided {.path {model_dir}} does not exist."
   )
 }
 
 build_solve_wrn <- function() {
   list(
     accuracy = c(
-      "Only {.emph {accuracy}} of variables accurate at at least 4 digit precision, less than {a_threshold}.",
-      "See {.arg accuracy_threshold} within {.fun teems::ems_option_set} to adjust the warning threshold."
+      "Only {.emph {accuracy}} of variables accurate to at least 4 digits, below the {a_threshold} threshold.",
+      "Adjust with {.arg accuracy_threshold} in {.fun teems::ems_option_set}."
     )
   )
 }
@@ -34,12 +35,12 @@ build_solve_wrn <- function() {
 build_solve_info <- function() {
   list(
     in_situ = "\"solve-in-situ\" mode activated.",
-    terminal_run = "{.arg terminal_run} mode has been selected enabling model runs outside of your R IDE or R script. The following steps are necessary to solve the model and compose outputs:",
-    terminal_run_steps = c("Run the following command in your OS terminal: {.field {solve_cmd}}",
+    terminal_run = "{.arg terminal_run} activated. To solve and compose outputs:",
+    terminal_run_steps = c("Run the above command in your OS terminal.",
                            "If errors are present in the terminal output during an ongoing run, it is possible to stop the relevant {.field {hsl}} process early according to your OS-specific system activity monitor.",
                            "Any error and/or singularity indicators will be present in the model diagnostic output: {.path {diag_out}}.",
                            "If no errors or singularities are detected, use the following expression to structure solver binary outputs: {.run ems_compose({cmf_path})}"),
-    accuracy = "{.emph {accuracy}} of variables accurate at at least 4 digit precision.",
+    accuracy = "{.emph {accuracy}} of variables accurate to at least 4 digits.",
     elapsed_time = "Elapsed time: {elapsed_time}"
   )
 }

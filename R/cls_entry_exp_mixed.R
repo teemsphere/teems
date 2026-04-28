464c97edcae8cab164ca02cc2de086715ca699d8
Matthew Cantele <m.cantele@unimelb.edu.au>
2026-04-29 11:21:58 +1200

    extend mixed entry expansion; formatting

--- R/cls_entry_exp_mixed.R
+++ R/cls_entry_exp_mixed.R
@@ -11,33 +11,38 @@
   entry_mixed <- sub(")", "", purrr::pluck(strsplit(cls_entry, "\\("), 1, 2))
   entry_mixed <- strsplit(entry_mixed, ",")[[1]]
   var_sets <- purrr::pluck(var_extract, "ls_upper_idx", attr(cls_entry, "var_name"))
-  
+  idx_sets <- purrr::pluck(var_extract, "ls_mixed_idx", attr(cls_entry, "var_name"))
   full_entry <- do.call(
     data.table::CJ,
     c(with(sets, mget(var_sets, ifnotfound = "")), sorted = FALSE)
   )
-  
+  data.table::setnames(full_entry, new = idx_sets)
+
   entry_mixed <- purrr::map(entry_mixed, function(e) {
     if (!grepl("\"", e)) {
-      # we could check subsets directly but need better set-subset mapping
+      # name search of sets more robust?
       with(sets, get(e))
     } else {
       utils::type.convert(gsub("\"", "", e), as.is = TRUE)
     }
   })
-  
+
   entry_mixed <- data.table::setnames(do.call(data.table::CJ, entry_mixed), var_sets)
+  data.table::setnames(entry_mixed, new = idx_sets)
   data.table::setkey(entry_mixed)
   if (nrow(data.table::fsetdiff(entry_mixed, full_entry)) %!=% 0L) {
     invalid_entries <- data.table::fsetdiff(entry_mixed, full_entry)
     n_invalid_entries <- nrow(invalid_entries)
+    invalid_entries <- utils::capture.output(print(invalid_entries))[-c(1:3)]
     .cli_action(model_err$mixed_invalid,
-                action = "abort",
-                call = call
+      action = "abort",
+      call = call
     )
   }
-  
+
   attr(cls_entry, "comp") <- strsplit(gsub("\"|\\)", "", pluck(strsplit(cls_entry, "\\("), 1, 2)), ",")[[1]]
+  data.table::setnames(entry_mixed, new = var_sets)
+  data.table::setkey(entry_mixed)
   attr(cls_entry, "ele") <- entry_mixed
   return(cls_entry)
 }

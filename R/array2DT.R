35c822212a665cc23f32e6be13a693373648f297
Matthew Cantele <m.cantele@unimelb.edu.au>
2026-04-29 11:21:35 +1200

    lowercase all dimension names and non-numeric scalar values on array → data.table conversion

--- R/array2DT.R
+++ R/array2DT.R
@@ -3,12 +3,17 @@
 #' @keywords internal
 #' @noRd
 .array2DT <- function(i_data) {
-  i_data <- lapply(i_data, function(arr) {
+  i_data <- lapply(i_data, \(arr) {
     dim_length <- length(dimnames(arr))
+
     if (dim_length %=% 0L) {
+      if (!is.numeric(arr)) {
+        arr <- tolower(arr)
+      }
       dt <- data.table::as.data.table(as.matrix(arr))
       data.table::setnames(dt, new = "Value")
     } else {
+      dimnames(arr) <- lapply(dimnames(arr), tolower)
       dt <- array2DF(arr)
       dt <- data.table::as.data.table(dt)
     }

3526bf87a9f90e4acb933eea915b3f99cd8b0417
Matthew Cantele <m.cantele@unimelb.edu.au>
2026-04-29 11:22:50 +1200

    add S3 dispatch for uniform and default year-to-timeset conversion

--- R/year2timeset.R
+++ R/year2timeset.R
@@ -1,30 +1,84 @@
+#' @noRd
+#' @keywords internal
+.year2time_set <- function(shk,
+                           sets,
+                           int_set_names,
+                           call) {
+  UseMethod(".year2time_set")
+}
+
 #' @importFrom purrr pluck
 #' @importFrom data.table setnames
 #' @importFrom utils capture.output
 #' 
 #' @noRd
 #' @keywords internal
-.year2time_set <- function(shk,
-                           sets,
-                           int_set_names) {
-  time_set_upper <- intersect(shk$ls_upper, int_set_names)
-  time_set <- shk$ls_mixed[match(time_set_upper, shk$ls_upper)]
-  CYRS <- attr(sets, "CYRS")
+#' 
+#' @method .year2time_set default
+#' @export
+.year2time_set.default <- function(shk,
+                                   sets,
+                                   int_set_names,
+                                   call) {
+  if ("Year" %in% shk$set) {
+    time_set_upper <- intersect(shk$ls_upper, int_set_names)
+    time_set <- shk$ls_mixed[match(time_set_upper, shk$ls_upper)]
+    CYRS <- attr(sets, "CYRS")
 
-  if (!all(shk$input$Year %in% CYRS$Value)) {
-    errant_year_tuples <- shk$input[!shk$input$Year %in% CYRS$Value]
-    n_errant_year_tuples <- nrow(errant_year_tuples)
-    errant_year_tuples <- utils::capture.output(print(errant_year_tuples))[-c(1, 2)]
-    .cli_action(
-      shk_err$invalid_year,
-      action = "abort",
-      call = attr(shk, "call")
-    )
+    if (!all(shk$input$Year %in% CYRS$Value)) {
+      errant_year_tuples <- shk$input[!shk$input$Year %in% CYRS$Value]
+      n_errant_year_tuples <- nrow(errant_year_tuples)
+      errant_year_tuples <- utils::capture.output(print(errant_year_tuples))[-c(1, 2)]
+      .cli_action(
+        shk_err$invalid_year,
+        action = c("abort", "inform"),
+        call = call
+      )
+    }
+    time_set_ele <- purrr::pluck(sets, "ele", time_set_upper)
+    r_idx <- match(shk$input$Year, CYRS$Value)
+    shk$input$Year <- CYRS$all_time[r_idx]
+    data.table::setnames(shk$input, "Year", time_set)
+    shk$set[match("Year", shk$set)] <- time_set
   }
-  time_set_ele <- purrr::pluck(sets, "ele", time_set_upper)
-  r_idx <- match(shk$input$Year, CYRS$Value)
-  shk$input$Year <- CYRS$all_time[r_idx]
-  data.table::setnames(shk$input, "Year", time_set)
-  shk$set[match("Year", shk$set)] <- time_set
+  return(shk)
+}
+
+#' @importFrom purrr pluck
+#' @noRd
+#' @keywords internal
+#' 
+#' @method .year2time_set uniform
+#' @export
+.year2time_set.uniform <- function(shk,
+                                   sets,
+                                   int_set_names,
+                                   call) {
+  
+  if ("Year" %in% names(shk$subset)) {
+    Year <- purrr::pluck(shk, "subset", "Year")
+    time_set_upper <- intersect(shk$ls_upper, int_set_names)
+    time_set <- shk$ls_mixed[match(time_set_upper, shk$ls_upper)]
+    if (time_set %in% names(shk$subset)) {
+      .cli_action(
+        shk_err$extra_col,
+        action = c("abort", "inform"),
+        call = call
+      )
+    }
+    
+    CYRS <- attr(sets, "CYRS")
+    if (!Year %in% CYRS$Value) {
+      CYRS <- CYRS$Value
+      .cli_action(
+        shk_err$uni_invalid_year,
+        action = c("abort", "inform"),
+        call = call
+      )
+    }
+    v_idx <- match(Year, CYRS$Value)
+    purrr::pluck(shk, "subset", "Year") <- purrr::pluck(sets, "ele", time_set_upper, v_idx)
+    names(shk$subset) <- sub("Year", time_set, names(shk$subset))
+  }
   return(shk)
 }

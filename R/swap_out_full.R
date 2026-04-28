72e1d50f653bc1a48caf47801ba4bbb26708d8a9
Matthew Cantele <m.cantele@unimelb.edu.au>
2026-04-29 11:18:55 +1200

    rename parameter swap_out → swap; apply idx-renamed setnames before fsetdiff; attribute call from swap object; add setnames import; fix indentation

--- R/swap_out_full.R
+++ R/swap_out_full.R
@@ -1,23 +1,22 @@
 #' @importFrom purrr pluck map_lgl map
-#' @importFrom data.table CJ rbindlist fsetdiff
-#' 
+#' @importFrom data.table CJ rbindlist fsetdiff setnames
+#'
 #' @keywords internal
 #' @noRd
 #' @method .swap_out full
 #' @export
-.swap_out.full <- function(swap_out,
+.swap_out.full <- function(swap,
                            closure,
                            sets,
                            var_name,
                            var_extract,
                            var_entries,
                            call) {
-
   if (length(var_entries) %=% 1L) {
     if (!inherits(var_entries[[1]], "full")) {
       .cli_action(swap_err$invalid_full,
-                  action = "abort",
-                  call = call
+        action = "abort",
+        call = call
       )
     }
 
@@ -29,14 +28,20 @@
       data.table::CJ,
       c(with(sets$ele, mget(var_sets, ifnotfound = "")), sorted = FALSE)
     )
+    idx_sets <- purrr::pluck(var_extract, "ls_mixed_idx", var_name)
+    data.table::setnames(full_var, new = idx_sets)
+    data.table::setnames(check, new = idx_sets)
     
     if (nrow(data.table::fsetdiff(full_var, check)) %!=% 0L) {
+      if (!is.null(attr(swap, "call"))) {
+        call <- attr(swap, "call")
+      }
       .cli_action(swap_err$invalid_full,
-                  action = "abort",
-                  call = call
+        action = c("abort", "inform"),
+        call = call
       )
     }
-    
+
     closure <- closure[!purrr::map_lgl(closure, function(c) {
       attr(c, "var_name") %=% var_name
     })]

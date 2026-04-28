0fd7b3762ae2b59615d33dfdfb2711870855e7cf
Matthew Cantele <m.cantele@unimelb.edu.au>
2026-04-29 11:22:16 +1200

    remove model_dir existence check; remove file_path_sans_ext import; formatting

--- R/get_output_paths.R
+++ R/get_output_paths.R
@@ -1,12 +1,9 @@
-#' @importFrom tools file_path_sans_ext
-#' 
 #' @keywords internal
 #' @noRd
 .get_output_paths <- function(cmf_path,
                               type,
-                              #select = NULL,
+                              # select = NULL,
                               call) {
-
   model_dir <- dirname(normalizePath(cmf_path))
   if ("tab_path" %in% names(attributes(cmf_path))) {
     tab_path <- attr(cmf_path, "tab_path")
@@ -17,25 +14,10 @@
     )
   }
 
-  if (!dir.exists(paths = model_dir)) {
-    .cli_action(compose_err$x_model_dir,
-      action = "abort",
-      call = call
-    )
-  }
-
   metadata_path <- file.path(model_dir, "metadata.rds")
-  
+
   if (!file.exists(metadata_path) || !inherits(readRDS(metadata_path), "teems_metadata")) {
     metadata_path <- NULL
-  }
-  
-  sol_mds <- file.path(model_dir, "out", "variables", "bin", "sol.mds")
-  if (!file.exists(sol_mds)) {
-    .cli_action(compose_err$x_var_out,
-      action = "abort",
-      call = call
-    )
   }
 
   set_paths <- list.files(

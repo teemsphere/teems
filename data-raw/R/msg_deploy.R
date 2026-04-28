2524e14801d4ab9f0bc6b9981a6427fcfe0b5ef7
Matthew Cantele <m.cantele@unimelb.edu.au>
2026-04-29 11:24:58 +1200

    add deploy_wrn builder; update message strings

--- data-raw/R/msg_deploy.R
+++ data-raw/R/msg_deploy.R
@@ -1,17 +1,35 @@
 build_deploy_err <- function() {
   list(
-    missing_header = "The following headers are designated as read-in but are missing from the loaded data: {.val {missing_headers}}.",
+    # test-ems_deploy.R: "ems_deploy errors when read-in headers not present in data"
+    missing_header = "Read-in headers missing from loaded data: {.val {missing_headers}}.",
+    # not in tests
     while_loop = "Construction of dependent sets has failed on: {null_sets}.",
-    missing_mapping = "Some model sets that are read as headers are missing mappings: {.field {m_map}}.",
-    nonreq_tsteps = "{.arg time_steps} have been provided yet no intertemporal sets have been detected in the provided model. See {.fun teems::ems_data}.",
-    missing_tsteps = "{.arg time_steps} have not been provided to an intertemporal model. See {.fun teems::ems_data}.",
-    data_set_mismatch = "The expected number of data entries on {.field {class(dt)[1]}} ({.val {expected}}) is not equal to the number found ({.val {nrow(dt)}}).",
-    invalid_write_dir = "The path provided for {.arg write_dir}, {.path {write_dir}}, does not exist and could not be created. Ensure the parent directory exists.",
+    # test-ems_deploy.R: "ems_deploy errors when read-in headers are missing mapping"
+    missing_mapping = "Some read-in model sets have no mappings: {.field {m_map}}.",
+    # test-ems_deploy.R: "ems_deploy errors when timesteps provided to static model"
+    nonreq_tsteps = "{.arg time_steps} provided but no intertemporal sets detected in the model. See {.fun teems::ems_data}.",
+    # test-ems_deploy.R: "ems_deploy errors when timesteps not provided to a dynamic model"
+    missing_tsteps = "{.arg time_steps} required for intertemporal models. See {.fun teems::ems_data}.",
+    # test-ems_deploy.R: "ems_deploy errors when set-calculated number of entries does not match a finalized data header"
+    data_set_mismatch = "{.field {class(dt)[1]}} has {.val {nrow(dt)}} entries; {.val {expected}} expected.",
+    # test-ems_deploy.R: "ems_deploy errors when write_dir does not exist"
+    invalid_write_dir = "{.path {write_dir}} does not exist and could not be created. Ensure the parent directory exists.",
+    # not in tests
     write_dir_not_writable = "The directory {.path {write_dir}} exists but is not writable.",
-    invalid_plus = "The set operator `+` was used where there are overlapping set elements {.field {d}}, violating the condition that the sets be disjoint.",
-    agg_missing_tup = "{tup$n} tuple{?s} in the provided input file for {.val {nme}} were missing: {.field {tup$missing}}.",
-    ele_invalid = "The closure entry tuple {.field {cls_entry}} is invalid under the current set mapping.",
-    mixed_invalid = "{n_invalid_entries} closure entry element{?s} in {.field {cls_entry}} do not belong to the respective variable sets: {invalid_entries}.",
-    subset_invalid = "Some entries from {.field {cls_entry}} do not belong to the respective variables sets indicating an invalid subset."
+    # not in tests
+    invalid_plus = "Set operator {.code +} requires disjoint sets; overlapping elements: {.field {d}}.",
+    # test-ems_deploy.R: "ems_deploy errors when aggregated inputs are incomplete"
+    agg_missing_tup = "{n} tuple{?s} in the provided input file for {.val {nme}} were missing: {.field {missing}}.",
+    # test-ems_deploy.R: "ems_deploy errors when shock_file and shock are both provided"
+    shk_file_shocks = c(
+      "No additional shocks are accepted if a shock file is provided."
+    )
+  )
+}
+
+build_deploy_wrn <- function() {
+  list(
+    # test-ems_deploy.R: "ems_deploy warns when creating write_dir"
+    mkdir = "Creating directory: {.path {write_dir}}."
   )
 }

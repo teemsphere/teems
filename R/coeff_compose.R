9f1500d95eaf9c1c9192849b9e3cf873360c3be0
Matthew Cantele <m.cantele@unimelb.edu.au>
2026-05-11 20:40:51 +1200

    delegate block parsing to .parse_coeff_block; remove map_lgl and setDT imports; rename col to col_nmes

--- R/coeff_compose.R
+++ R/coeff_compose.R
@@ -1,7 +1,5 @@
-#' @importFrom purrr map_lgl pluck pmap transpose simplify
-#' @importFrom tibble tibble
-#' @importFrom data.table setDT
-#' 
+#' @importFrom purrr pluck pmap transpose simplify
+#' @importFrom tibble tibble add_column
 #' @keywords internal
 #' @noRd
 .compose_coeff <- function(paths,
@@ -9,29 +7,28 @@
                            sets,
                            time_steps,
                            call) {
-  
+
   ls_data <- lapply(paths, readLines)
 
   list_coeff <- lapply(
     ls_data,
     FUN = function(dat) {
       lead <- dat[1]
-      dat <- dat[-length(dat)]
-      dat <- dat[-1]
-      name <- purrr::pluck(strsplit(lead, split = '"'), 1, 2)
+      dat  <- dat[-length(dat)]
+      dat  <- dat[-1]
+      name  <- purrr::pluck(strsplit(lead, split = '"'), 1, 2)
       label <- purrr::pluck(strsplit(lead, split = '"'), 1, 4)
-      dim <- as.integer(strsplit(strsplit(lead, split = "Real|Integer")[[1]][1], " ")[[1]])
-      coeff <- list(name, label, dim, dat)
-      return(coeff)
+      dim   <- as.integer(strsplit(strsplit(lead, split = "Real|Integer")[[1]][1], " ")[[1]])
+      list(name, label, dim, dat)
     }
   )
 
   transposed <- purrr::transpose(list_coeff)
 
   coeff_tib <- tibble::tibble(
-    name = purrr::simplify(transposed[[1]], .type = "character"),
-    label = purrr::simplify(transposed[[2]], .type = "character"),
-    dim = purrr::simplify(transposed[[3]], .type = "character"),
+    name    = purrr::simplify(transposed[[1]], .type = "character"),
+    label   = purrr::simplify(transposed[[2]], .type = "character"),
+    dim     = purrr::simplify(transposed[[3]], .type = "character"),
     ls_data = transposed[[4]]
   )
 
@@ -47,83 +44,18 @@
     )
   }
 
-  r_idx <- match(
-    coeff_tib$name,
-    coeff_extract$name
-  )
-
+  r_idx <- match(coeff_tib$name, coeff_extract$name)
   coeff_tib$set_nmes <- coeff_extract$ls_mixed_idx[r_idx]
 
   coeff_tib$dat <- purrr::pmap(
     .l = list(
-      dimen = coeff_tib$dim,
-      col = coeff_tib$set_nmes,
-      num_ls = coeff_tib$ls_data
+      dimen    = coeff_tib$dim,
+      col_nmes = coeff_tib$set_nmes,
+      num_ls   = coeff_tib$ls_data
     ),
     .f = function(dimen, col_nmes, num_ls) {
-      dim_length <- length(dimen)
-
-      if (col_nmes %!=% NA_character_) {
-        plain_col <- .dock_tail(string = col_nmes)
-        r_idx <- match(plain_col, names(sets))
-        setele <- sets[r_idx]
-        if (is.null(setele)) {
-          .cli_action(compose_err$invalid_coeff_set,
-            action = "abort",
-            call = call
-          )
-        }
-      }
-
-      if (dim_length %=% 1L) {
-        if (col_nmes %=% NA_character_) {
-          df <- data.frame(Value = as.numeric(num_ls))
-        } else {
-          df <- data.frame(setele, Value = as.numeric(num_ls))
-          colnames(df)[1] <- col_nmes
-        }
-      } else if (dim_length %=% 2L) {
-        flat_vec <- as.numeric(unlist(strsplit(num_ls, split = ",")))
-        arr <- t(array(
-          flat_vec,
-          rev(dimen)
-        ))
-        dimnames(arr) <- setele
-        names(dimnames(arr)) <- col_nmes
-        df <- array2DF(arr)
-      } else {
-        num_ls <- lapply(num_ls, function(r) {
-          as.numeric(unlist(strsplit(r, split = ",")))
-        })
-        null_markers <- purrr::map_lgl(
-          num_ls,
-          function(l) {
-            l %=% numeric(0)
-          }
-        )
-        # not a pretty algo
-        group_ids <- cumsum(null_markers)
-        split_ls <- split(num_ls[!null_markers], f = group_ids[!null_markers])
-        ls_mat <- lapply(
-          split_ls,
-          FUN = function(l) {
-            t(array(unlist(l), rev(dimen[c(1, 2)])))
-          }
-        )
-        arr <- array(unlist(ls_mat), dimen)
-        dimnames(arr) <- setele
-        names(dimnames(arr)) <- col_nmes
-        df <- array2DF(arr)
-      }
-      if (col_nmes %!=% NA_character_) {
-        if (any(purrr::map_lgl(setele, inherits, "numeric"))) {
-          int_col <- colnames(df)[purrr::map_lgl(setele, inherits, "numeric")]
-          df[int_col] <- lapply(df[int_col], as.numeric)
-        }
-        data.table::setDT(df, key = col_nmes)
-      } else {
-        data.table::setDT(df)
-      }
+      dt <- .parse_coeff_block(dimen, col_nmes, num_ls, sets, call)
+      return(dt)
     }
   )
 

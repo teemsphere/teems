5d2d8a19ef30b8b1cd4080928031c035032bfab6
Matthew Cantele <m.cantele@unimelb.edu.au>
2026-05-11 20:41:51 +1200

    add same_format error message

--- data-raw/R/msg_convert.R
+++ data-raw/R/msg_convert.R
@@ -1,5 +1,6 @@
 build_convert_err <- function() {
   list(
-    format = "{.arg target} set to {.field {target}} but data appears to already be this format."
+    format = "{.arg target} set to {.field {target}} but data appears to already be this format.",
+    same_format = "{.arg target} must differ from {.arg origin}." 
   )
 }

3523e2595da70dd22da9f1048efbf1996241d76a
Matthew Cantele <m.cantele@unimelb.edu.au>
2026-05-11 20:41:46 +1200

    add data_dir deletion on load; fix chained assignment so cache_dir and data_dir are independent

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,14 @@
 #' @noRd
 .onLoad <- function(libname, pkgname) {
   cache_dir <- tools::R_user_dir("teems", "cache")
+  data_dir  <- tools::R_user_dir("teems", "data")
 
   if (dir.exists(cache_dir)) {
     unlink(cache_dir, recursive = TRUE, force = TRUE)
+  }
+
+  if (dir.exists(data_dir)) {
+    unlink(data_dir, recursive = TRUE, force = TRUE)
   }
 
   invisible(NULL)
